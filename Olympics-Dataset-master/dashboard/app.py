35b5ac7a89ef111cac85c1db05320e4ac16d15eb
@Mikist <mikeochango3@gmail.com>
2024-08-22 23:45:44 +0300

    design UI part 1

--- Olympics-Dataset-master/dashboard/app.py
+++ Olympics-Dataset-master/dashboard/app.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from shiny.express import ui, input, render
+from shiny import reactive
 
 ui.page_opts(fillable=True)
 
 with ui.layout_sidebar():
-    with ui.sidebr():
-        ui.input_select("Country", "Select a country")
+    with ui.sidebar(open='open'):
+        ui.input_select("Country", "Select a country",['USA',"IND","MEX"])
+        ui.input_checkbox("winter","Include winter games?",True)
+        ui.input_checkbox("medalists","Only include medalists?", True)
 
 
-        @render.plot(aalt="Ahistogram")
+        @render.data_frame
         def histogram():
             np.random.seed(19680801)
-            x = 100 + 15 * np.randn(437)
+            x = 100 + 15 * np.random.randn(437)
             plt.hist(x, input.n(), density=True)
