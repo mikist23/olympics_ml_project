3dad376586e843cba127f12919ce0429d405cac2
@Mikist <mikeochango3@gmail.com>
2024-08-23 00:30:32 +0300

    load all the countries  and create some cards

--- Olympics-Dataset-master/dashboard/app.py
+++ Olympics-Dataset-master/dashboard/app.py
@@ -8,23 +8,51 @@ from pathlib import Path
 
 ui.page_opts(fillable=True)
 
-
+# get all NOCs
+directory = Path(__file__).parent.parent
+nocs = pd.read_csv(f'{directory}/clean-data/noc_regions.csv')
     
 
 with ui.layout_sidebar():
     with ui.sidebar(open='open'):
-        ui.input_select("Country", "Select a country",['USA',"IND","MEX"])
+        ui.input_select("country", "Select a country",nocs.NOC.unique().tolist())
         ui.input_checkbox("winter","Include winter games?",True)
         ui.input_checkbox("medalists","Only include medalists?", True)
 
 
-    @render.data_frame
-    def results():
-        return  results_df().head(100)
+
+    with ui.card():
+        "Medals"
+        # print medals by country
+
+    with ui.card():
+        "Heatmap of athletes"
+        # Heatmap
+
+
+    with ui.card():
+        @render.data_frame
+        def results():
+            return  results_df().head(100)
     
+
+
+@reactive.calc
+def bios_df():
+    directory = Path(__file__).parent.parent
+    df = pd.read_csv(f'{directory}/clean-data/bios_locs.csv')
+    return df
+
 
 @reactive.calc
 def results_df():
     directory = Path(__file__).parent.parent
-    df = pd.read_csv(f'{directory}/clean-data/bios_locs.csv')
+    df = pd.read_csv(f'{directory}/clean-data/results.csv')
+    df = df[df['noc'] == input.country()]
+
+    if not input.winter():
+        df = df[df['type'] == 'Summer']
+    if input.medalists():
+        df = df[df['medal'].notna()]
+
     return df
